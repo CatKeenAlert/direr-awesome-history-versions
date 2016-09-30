48b72482488a7d255be579ce7efd177f1355bb52
catkeenalert <catkeenalert@gmail.com>
2016-09-30 21:55:33 +0800

    最初的ed996c7版本的定格,将  git remote add awesome  git@github.com:CatKeenAlert/direr-awesome-history-versions.git

---
+++ IndexController.class.php
@@ -0,0 +1,20 @@
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+echo '当前网站根目录的绝对路径是'.$root.'<br>';
+if(!($root == $abs_current_dir))
+{
+    $length = strlen($root);
+    $current_dir_in_root = substr($abs_current_dir, $length);
+    //echo '当前目录在网站根目录中的目录： '.$current_dir_in_root;
+}else{
+    //echo '当前目录就是网站根目录或是符号连接。';
+    $current_dir_in_root = '';
+}
+echo '当前项目绝对路径是： '.$APP_PATH;
+$url_root = $_SERVER['SERVER_NAME'];
+$current_dir_mid = 'http://'.$url_root.$current_dir_in_root;
+echo '<br>';
+echo '当前目录对应的URL是： '.$current_dir_mid;
+echo '<br>------------以上是中间的php文件的输出--------------<br><br>';
+require $APP_PATH."/FilestableModel.class.php";
+?>
